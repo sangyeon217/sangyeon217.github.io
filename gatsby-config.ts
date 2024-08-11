c6aed55d873729d6323dfc0fe727d230aa3b21ae
Sangyeon <sangyeon217@gmail.com>
2024-08-11 14:58:48 +0900

    Gatsby 초기 세팅

---
+++ gatsby-config.ts
@@ -0,0 +1,40 @@
+import type { GatsbyConfig } from "gatsby";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Sangyeon Tech Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  jsxRuntime: "automatic",
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
+
+export default config;
