5628e0ec4124c6bb58c384a4006e89c631c74cfb
Sangyeon <sangyeon217@gmail.com>
2024-08-17 18:55:24 +0900

    레이아웃 컴포넌트 전역 스타일 설정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,4 +1,23 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('<https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css>');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard';
+  }
+
+  html,
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper {
+    min-height: 100%;
+    height: 100%;
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,5 +28,9 @@ const Wrapper = styled.div`
 `;
 
 export default function Layout() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <GlobalStyle />
+    </Wrapper>
+  );
 }
