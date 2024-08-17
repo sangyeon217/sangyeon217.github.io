7f5af365ee0a835d898952f25868c1b7f8ee67ef
Sangyeon <sangyeon217@gmail.com>
2024-08-17 18:54:48 +0900

    레이아웃 컴포넌트 추가

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  min-height: 100%;
+  margin: 0 auto;
+`;
+
+export default function Layout() {
+  return <Wrapper></Wrapper>;
+}
