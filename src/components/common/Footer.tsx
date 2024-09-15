75b3f03b44e5d097ea576c6be90f932f85eb7cef
Sangyeon <sangyeon217@gmail.com>
2024-09-15 15:56:50 +0900

    푸터 문구 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled.footer`
   font-size: 15px;
   text-align: center;
   line-height: 1.5;
+  color: rgba(98, 92, 96, 0.8);
 
   @media (max-width: 768px) {
     font-size: 12px;
@@ -17,7 +18,7 @@ const Wrapper = styled.footer`
 export default function Footer() {
   return (
     <Wrapper>
-      <div>Copyright © 2024 All rights reserved by Sangyeon Song</div>
+      <div>Copyright © 2024. Sangyeon Song. All rights reserved.</div>
     </Wrapper>
   );
 }
