5b5a064f329c3f26ebac3edf00f42b85fd82c154
Sangyeon <sangyeon217@gmail.com>
2024-09-25 21:47:26 +0900

    Buy me a Coffee 위젯 추가 #7

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -11,10 +11,28 @@ const HeadComponents = [
   />,
 ];
 
+const PostBodyComponents = [
+  <script
+    key="buyMeACoffee"
+    data-name="BMC-Widget"
+    data-cfasync="false"
+    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+    data-id="sangyeon217"
+    data-description="Support me on Buy me a coffee!"
+    data-message=""
+    data-color="#5F7FFF"
+    data-position="Right"
+    data-x_margin="18"
+    data-y_margin="18"
+  ></script>,
+];
+
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({
   setHeadComponents,
+  setPostBodyComponents,
 }) => {
   setHeadComponents(HeadComponents);
+  setPostBodyComponents(PostBodyComponents);
 };
 
 export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({
